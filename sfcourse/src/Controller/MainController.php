2095f277601a5b7d606c0c7b571c748e6f3dd742
unknown <janblaszczyk93@gmail.com>
2020-10-25 19:55:56 +0100

    sfcourse

---
+++ sfcourse/src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="main")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+}
