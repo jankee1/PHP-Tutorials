c2662c6f099c31b42f303bb32a5e13c8f3e05da3
unknown <janblaszczyk93@gmail.com>
2020-12-06 13:23:57 +0100

    06.12.2020 added functionality:forms

--- sfcourse/src/Controller/PostController.php
+++ sfcourse/src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,11 +17,15 @@ class PostController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
         ]);
     }
 
@@ -38,10 +43,36 @@ class PostController extends AbstractController
         $em = $this->getDoctrine()->getManager();
 
         $em->persist($post);
-        
+
         $em->flush();
         //return a response
 
-        return new Response('Post was created');
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+    /**
+     * @Route("/show/{id}", name="show")
+     * @return Response
+     */
+    public function show($id, PostRepository $postRepository) {
+        $post = $postRepository->find($id);
+        // dump($post); die;
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @return Response
+     */
+    public function remove($id, PostRepository $postRepository) {
+        $post = $postRepository->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'Post was removed');
+
+        return $this->redirect($this->generateUrl('post.index'));
     }
 }
