8193e8c6f50e4bbdea0c1277fc024bf1796056b0
unknown <janblaszczyk93@gmail.com>
2020-12-09 20:09:26 +0100

    09.12.2020 added functionality: logout

---
+++ sfcourse/src/Controller/RegistrationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+
+        $form = $this->createFormBuilder()
+        ->add('username')
+        ->add('password', RepeatedType::class, [
+            'type' =>PasswordType::class,
+            'required'=> true,
+            'first_options' => ['label' => 'Password'],
+            'second_options' => ['label' => 'Repeat Password']
+        ])
+        ->add('register', SubmitType::class, [
+            'attr' => [
+                'class' => 'btn btn-success float-right'
+            ]
+        ])
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $data['password'])
+            );
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('app_login'));
+    }
+}
--- sfcourse/src/Security/CustomerAuthenticator.php
+++ sfcourse/src/Security/CustomerAuthenticator.php
@@ -97,7 +97,8 @@ class CustomerAuthenticator extends AbstractFormLoginAuthenticator implements Pa
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('post.index'));
     }
 
     protected function getLoginUrl()
