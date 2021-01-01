141d8289e87eda565ad736232c9c99d3f088e9d9
Jan <janblaszczyk93@gmail.com>
2021-01-01 21:19:54 +0100

    01.01.2021 added html markup

--- cauldron_overflow/src/Controller/QuestionController.php
+++ cauldron_overflow/src/Controller/QuestionController.php
@@ -2,22 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class QuestionController extends AbstractController
-{
-    #[Route('/', name: 'homepage')]
-    public function homepage(): Response
+{    
+    #[Route('/', name:'homepage')]
+    function homepage(): Response
     {
-        return new Response('some text');
+        return $this->Render('question/homepage.html.twig');
     }
 
-    #[Route('/questions/{slug}', name: 'questions')]
-    public function show($slug): Response
+    #[Route('/questions/{slug}', name:'questions')]
+    function show($slug): Response
     {
-        $sql = 
 
         $answers = [
             'Answer no 1',
@@ -29,10 +28,12 @@ class QuestionController extends AbstractController
 
         return $this->render('question/show.html.twig', [
             'question' => sprintf("how to tie shoes %s !", $slug),
-            'answers' => $answers
+            'answers' => $answers,
         ]);
+
         // return new Response(sprintf(
         //     'how tie shoes "%s" !', $slug
         // ));
     }
+
 }
