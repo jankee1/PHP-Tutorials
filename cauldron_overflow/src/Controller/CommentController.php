dc7c3e7c701c0b6620c40f6640e57d66a6c8fe54
Jan <janblaszczyk93@gmail.com>
2021-01-02 15:05:48 +0100

    02.01.2021 having fun with Environment

--- cauldron_overflow/src/Controller/CommentController.php
+++ cauldron_overflow/src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comments/{id}/vote/{direction}', name: 'app_comment')]
-    public function commentVote($id, $direction)
+    #[Route('/comments/{id}/vote/{direction<up|down>}', name: 'app_comment', methods: ['POST'])]
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
-        if($direction === 'up') 
+        if($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7,100);
-        else
+        }
+        else{
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0,5);
+        }
+            
 
         return $this->json([
             'votes' => $currentVoteCount
