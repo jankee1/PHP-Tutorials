d7acbaacb704b57cbe5d7f8dafe1b90386cccf3d
Jan <janblaszczyk93@gmail.com>
2021-01-05 19:44:15 +0100

    05.01.2021 finished tutorial 1/4

--- real-time-chat-app/src/Repository/ConversationRepository.php
+++ real-time-chat-app/src/Repository/ConversationRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Conversation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
+use function Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Conversation|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +19,34 @@ class ConversationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conversation::class);
+    }
+
+    public function findConversationByParticipants(int $otherUserId, int $myId)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb
+          ->select($qb->expr()->count('p.conversation'))
+          ->innerJoin('c.participants', 'p')
+          ->where(
+              $qb->expr()->orX(
+                  $qb->expr()->eq('p.user', ':me'),
+                  $qb->expr()->eq('p.user', ':otherUser')
+              )
+          )
+          ->groupBy('p.conversation')
+          ->having(
+              $qb->expr()->eq(
+                  $qb->expr()->count('p.conversation'),
+                  2
+                )
+            )
+          ->setParameters([
+            'me' => $myId,
+            'otherUser' => $otherUserId
+          ])
+        ;
+
+        return $qb->getQuery()->getResult();
     }
 
     // /**
