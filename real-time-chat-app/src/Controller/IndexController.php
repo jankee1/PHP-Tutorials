dcbc9dfb4ed0dd2465df151ed33aa948de5cfd34
Jan <janblaszczyk93@gmail.com>
2021-01-09 13:15:15 +0100

    09.01.2021 update

--- real-time-chat-app/src/Controller/IndexController.php
+++ real-time-chat-app/src/Controller/IndexController.php
@@ -5,12 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Locbucci\JWT\Builder;
+use Locbucci\JWT\Signer\Hmac\Sha256;
+use Locbucci\JWT\Key;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
     {
+        $username = $this->getUser()->getUsername();
+        $token = (new Builder)
+          ->withClaim('mercure', ['subscribe' => [sprintf("%s", $username)]])
+          ->getToken(
+              new Sha256(),
+              new Key($this->getParameter('mercure_secret_key'))
+            )
+          ;
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
