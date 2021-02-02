bbaad372b7fe5adac0dcd6286ddbc5a6c67995da
Jan <jan.blaszczyk@protonmail.com>
2021-02-02 18:29:09 +0100

    02-02-2021_redirections

--- Symfony_4_and_5-Todo_list/src/Controller/DefaultController.php
+++ Symfony_4_and_5-Todo_list/src/Controller/DefaultController.php
@@ -4,15 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'default')]
-    public function index(): Response
+    #[Route('/default/{name?}', name: 'default')]
+    public function index($name)
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        // return $this->render('default/index.html.twig', [
+        //     'controller_name' => 'DefaultController',
+        // ]);
+
+        // return new Response("Hello $name");
+
+        // return new RedirectResponse('http://stackoverflow.com');
+
+        return $this->redirectToRoute('default2');
+    }
+    #[Route('/default2/', name: 'default2')]
+    public function index2()
+    {
+        return new Response('hello');
     }
 }
