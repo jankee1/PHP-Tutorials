349b50a873f634373b03912984ba74d03113ea31
Jan <jan.blaszczyk@protonmail.com>
2021-02-03 19:56:25 +0100

    03-02-2021_service_containers_and_Services_p1

--- Symfony_4_and_5-Todo_list/src/Controller/DefaultController.php
+++ Symfony_4_and_5-Todo_list/src/Controller/DefaultController.php
@@ -7,11 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Services\GiftsService;
 
 class DefaultController extends AbstractController
 {
+
+    public function __construct(GiftsService $giftsservice)
+    {
+        $giftsservice->gifts = ['a', 'b', 'c', 'd'];
+    }
     #[Route('/', name: 'default')]
-    public function index()
+    public function index(GiftsService $giftsservice)
     {
         // return $this->render('default/index.html.twig', [
         //     'controller_name' => 'DefaultController',
@@ -22,11 +28,14 @@ class DefaultController extends AbstractController
         // return new RedirectResponse('http://stackoverflow.com');
         // return $this->redirectToRoute('default2');
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
-
+        // $gifts = ['flowers', 'car', 'piano', 'money'];
+        //
+        // shuffle($gifts);
 
         return $this->render('default/index.html.twig', [
           'controller_name' => 'DefaultController',
-          'users' => $users
+          'users' => $users,
+          'random_gift' => $giftsservice->gifts
         ]);
     }
     #[Route('/default2/', name: 'default2')]
