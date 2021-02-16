b8646532ba66e64123af0f350494a945cbb3b470
Jan <jan.blaszczyk@protonmail.com>
2021-02-16 19:53:30 +0100

    16-02-2021_forms_validations_and_files_upload

--- Symfony_4_and_5-General_course/src/Form/VideoFormType.php
+++ Symfony_4_and_5-General_course/src/Form/VideoFormType.php
@@ -9,6 +9,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class VideoFormType extends AbstractType
 {
@@ -23,18 +27,38 @@ class VideoFormType extends AbstractType
               'data' => 'Example title',
               'required' => false
             ])
-            ->add('created_at', DateType::class, [
-              'label' => 'Set date',
-              'widget' => 'single_text'
+            // ->add('created_at', DateType::class, [
+            //   'label' => 'Set date',
+            //   'widget' => 'single_text'
+            // ])
+            ->add('format')
+            ->add('duration')
+            ->add('agreeTerms', CheckboxType::class, [
+              'label' => 'Agree ?',
+              'mapped' => false //very important!!!! setting it to false we make sure that it is not mapped to entity when we will try to send a form
+            ])
+            ->add('file', FileType::class, [
+              'label' => 'Video (MP4 file)'
             ])
             ->add('save', SubmitType::class, [
               'label' => 'Add a video'
             ])
-            ->add('format')
-            ->add('duration')
             // ->add('author')
             // ->add('user')
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+          $video = $event->getData();
+          $form = $event->getForm();
+
+          if(!$video || null === $video->getId()) {
+            $form->add('created_at', DateType::class, [
+              'label' => 'Set Date',
+              'widget' => 'single_text'
+            ]);
+          }
+        });
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
