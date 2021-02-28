d19f08e5f9ccd07aa71c5e0c980caff5dbd2af33
Jan <jan.blaszczyk@protonmail.com>
2021-02-28 14:05:16 +0100

    28-02-2021_authorization

--- Symfony_4_and_5-General_course/src/Controller/DefaultController.php
+++ Symfony_4_and_5-General_course/src/Controller/DefaultController.php
@@ -338,111 +338,144 @@ class DefaultController extends AbstractController
   //       'random_gift' => $gifts->gifts
   //     ]);
 
-  #[Route('/home/{id?}', name: 'home')]
-  public function index(Request $request, \Swift_Mailer $mailer, UserPasswordEncoderInterface $passwordEncoder )
-  {
-      // $cache = new FileSystemAdapter();
-      // $posts = $cache->getItem('database.get_posts');
-      // if(!$posts->isHit()) {
-      //   $posts_from_db = ['post1', 'post2', 'post3', 'post4' ];
-      //   dump('Connected with database...');
-      //   $posts->set(serialize($posts_from_db));
-      //   $posts->expiresAfter(5);
-      //   $cache->save($posts);
-      // }
-      // $cache->deleteItem('database.get_posts');
-      // $cache->clear();
-      // dump(unserialize($posts->get()));
-      // $cache = new TagAwareAdapter(
-      //   new FileSystemAdapter()
-      // );
-      //
-      // $acer = $cache->getItem('acer');
-      // $dell = $cache->getItem('dell');
-      // $ibm = $cache->getItem('ibm');
-      // $apple = $cache->getItem('apple');
-      //
-      // if(!$acer->isHit()) {
-      //   $acer_from_db = 'acer laptop';
-      //   $acer->set($acer_from_db);
-      //   $acer->tag(['computers', 'laptops', 'acer']);
-      //   $cache->save($acer);
-      //   dump('acer from db');
-      // }
-      //
-      // if(!$dell->isHit()) {
-      //   $dell_from_db = 'dell desktops';
-      //   $dell->set($dell_from_db);
-      //   $dell->tag(['computers', 'desktops', 'dell']);
-      //   $cache->save($dell);
-      //   dump('dell from db hehe');
-      // }
-      //
-      // $cache->invalidateTags(['acer']);
-      //
-      // dump($acer->get());
-      // dump($dell->get());
-      // dump($ibm->get());
-      // dump($apple->get());
-      // $video->setTitle('write a blog post');
-      // $video->setCreatedAt(new \DateTime('tomorrow'));
+  // #[Route('/home/{id?}', name: 'home')]
+  // public function index(Request $request, \Swift_Mailer $mailer, UserPasswordEncoderInterface $passwordEncoder )
+  // {
+  //     // $cache = new FileSystemAdapter();
+  //     // $posts = $cache->getItem('database.get_posts');
+  //     // if(!$posts->isHit()) {
+  //     //   $posts_from_db = ['post1', 'post2', 'post3', 'post4' ];
+  //     //   dump('Connected with database...');
+  //     //   $posts->set(serialize($posts_from_db));
+  //     //   $posts->expiresAfter(5);
+  //     //   $cache->save($posts);
+  //     // }
+  //     // $cache->deleteItem('database.get_posts');
+  //     // $cache->clear();
+  //     // dump(unserialize($posts->get()));
+  //     // $cache = new TagAwareAdapter(
+  //     //   new FileSystemAdapter()
+  //     // );
+  //     //
+  //     // $acer = $cache->getItem('acer');
+  //     // $dell = $cache->getItem('dell');
+  //     // $ibm = $cache->getItem('ibm');
+  //     // $apple = $cache->getItem('apple');
+  //     //
+  //     // if(!$acer->isHit()) {
+  //     //   $acer_from_db = 'acer laptop';
+  //     //   $acer->set($acer_from_db);
+  //     //   $acer->tag(['computers', 'laptops', 'acer']);
+  //     //   $cache->save($acer);
+  //     //   dump('acer from db');
+  //     // }
+  //     //
+  //     // if(!$dell->isHit()) {
+  //     //   $dell_from_db = 'dell desktops';
+  //     //   $dell->set($dell_from_db);
+  //     //   $dell->tag(['computers', 'desktops', 'dell']);
+  //     //   $cache->save($dell);
+  //     //   dump('dell from db hehe');
+  //     // }
+  //     //
+  //     // $cache->invalidateTags(['acer']);
+  //     //
+  //     // dump($acer->get());
+  //     // dump($dell->get());
+  //     // dump($ibm->get());
+  //     // dump($apple->get());
+  //     // $video->setTitle('write a blog post');
+  //     // $video->setCreatedAt(new \DateTime('tomorrow'));
+  //
+  //     // $em = $this->getDoctrine()->getManager();
+  //     // $video = new Video();
+  //     // // $videos = $em->getRepository(Video::class)->findAll();
+  //     // // dump($videos);
+  //     // $form = $this->createForm(VideoFormType::class, $video);
+  //     // $form->handleRequest($request);
+  //     // if($form->isSubmitted() && $form->isValid())  {
+  //     //   // dump($form->getData());
+  //     //   $file = $form->get('file')->getData();
+  //     //   $fileName = sha1(random_bytes(14) . '.' . $file->guessExtension());
+  //     //   $file->move(
+  //     //     $this->getParameter('video_directory'),
+  //     //     $fileName
+  //     //   );
+  //     //   $video->setFile($fileName);
+  //     //   $em->persist($video);
+  //     //   $em->flush();
+  //     //   return $this->redirectToRoute('home');
+  //     // }
+  //     // $message = (new \Swift_Message('HelloEmail'))
+  //     //   ->setFrom('send@example.com')
+  //     //   ->setTo('recipient@example.com')
+  //     //   ->setBody(
+  //     //     $this->renderView(
+  //     //       'emails/registration.html.twig',
+  //     //       ['name' => 'Robertice']
+  //     //     ), 'text/html'
+  //     //     )
+  //     // ;
+  //     // $mailer->send($message);
+  //     $em = $this->getDoctrine()->getManager();
+  //     $users = $em->getRepository(SecurityUser::class)->findAll();
+  //     dump($users);
+  //
+  //     $user = new SecurityUser();
+  //     $form = $this->createForm(RegisterUserType::class, $user);
+  //     $form->handleRequest($request);
+  //
+  //     if($form->isSubmitted() && $form->isValid()){
+  //       $user->setPassword(
+  //         $passwordEncoder->encodePassword($user, $form->get('password')->getData())
+  //       );
+  //       $user->setEmail($form->get('email')->getData());
+  //       $em = $this->getDoctrine()->getManager();
+  //       $em->persist($user);
+  //       $em->flush();
+  //
+  //       return $this->redirectToRoute('home');
+  //     }
+  //
+  //     return $this->render('default/index.html.twig', [
+  //       'controller_name' => 'DefaultController',
+  //       // 'form1' => $form->createView()
+  //       'form' => $form->createView()
+  //     ]);
+  // }
 
-      // $em = $this->getDoctrine()->getManager();
-      // $video = new Video();
-      // // $videos = $em->getRepository(Video::class)->findAll();
-      // // dump($videos);
-      // $form = $this->createForm(VideoFormType::class, $video);
-      // $form->handleRequest($request);
-      // if($form->isSubmitted() && $form->isValid())  {
-      //   // dump($form->getData());
-      //   $file = $form->get('file')->getData();
-      //   $fileName = sha1(random_bytes(14) . '.' . $file->guessExtension());
-      //   $file->move(
-      //     $this->getParameter('video_directory'),
-      //     $fileName
-      //   );
-      //   $video->setFile($fileName);
-      //   $em->persist($video);
-      //   $em->flush();
-      //   return $this->redirectToRoute('home');
-      // }
-      // $message = (new \Swift_Message('HelloEmail'))
-      //   ->setFrom('send@example.com')
-      //   ->setTo('recipient@example.com')
-      //   ->setBody(
-      //     $this->renderView(
-      //       'emails/registration.html.twig',
-      //       ['name' => 'Robertice']
-      //     ), 'text/html'
-      //     )
-      // ;
-      // $mailer->send($message);
+  #[Route('/home', name: 'home')]
+  public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+  {
       $em = $this->getDoctrine()->getManager();
       $users = $em->getRepository(SecurityUser::class)->findAll();
       dump($users);
 
-      $user = new SecurityUser();
-      $form = $this->createForm(RegisterUserType::class, $user);
-      $form->handleRequest($request);
+      // $user = new SecurityUser();
+      // $user->setEmail('admin@admin.com');
+      // $password = $passwordEncoder->encodePassword($user, 'passw');
+      // $user->setPassword($password);
+      // $user->setRoles(['ROLE_ADMIN']);
+      //
+      // $video = new Video();
+      // $video->setTitle('video title');
+      // $video->setFile('video path');
+      // $video->setCreatedAt(new \DateTime());
+      // // $em->persist($video);
+      // //
+      // // $user->addVideo($video);
+      // $em->persist($user);
+      // $em->flush();
+      //
+      // dump($user->getId());
+      // dump($video->getId());
 
-      if($form->isSubmitted() && $form->isValid()){
-        $user->setPassword(
-          $passwordEncoder->encodePassword($user, $form->get('password')->getData())
-        );
-        $user->setEmail($form->get('email')->getData());
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
-        $em->flush();
-
-        return $this->redirectToRoute('home');
-      }
 
       return $this->render('default/index.html.twig', [
         'controller_name' => 'DefaultController',
-        // 'form1' => $form->createView()
-        'form' => $form->createView()
       ]);
   }
+
 
   #[Route('/login', name: 'login')]
   public function login(AuthenticationUtils $authenticationUtils)
