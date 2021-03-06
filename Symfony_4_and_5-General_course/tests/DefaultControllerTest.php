2d35be22d62143eb435cd61466f480d78a318bb9
Jan <jan.blaszczyk@protonmail.com>
2021-03-06 14:49:36 +0100

    06-03-2021_functional_tests

--- Symfony_4_and_5-General_course/tests/DefaultControllerTest.php
+++ Symfony_4_and_5-General_course/tests/DefaultControllerTest.php
@@ -6,29 +6,56 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    /**
+    * @dataProvider provideUrls
+    *
+    */
+    public function testSomething($url)
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/home');
+        $crawler = $client->request('GET', $url);
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello');
+        // $this->assertResponseIsSuccessful();
+        // $this->assertSelectorTextContains('h1', 'Hello');
 
         // $this->assertSame(200, $client->getResponse()->getStatusCode());
         // $this->assertContains('Hello', $crawler->filter('h1')->text());
-        $this->assertGreaterThan(
-          0,
-          $crawler->filter("html:contains('Hello')")->count()
-        );
-        // $this->assertGreaterThan(0, $crawler->filter('h1.class')->count());
-        $this->assertCount(1, $crawler->filter("h1"));
-        $this->assertTrue(
-          $client->getResponse()->headers->contains(
-            'Content-Type',
-            'application/json'
-          ),
-          "the 'Content-Type' header is 'application/json'" //optional message shown on failuare
-        );
-        $this->assertContains('foo', $client->getResponse()->getContent());
+        // $this->assertGreaterThan(
+        //   0,
+        //   $crawler->filter("html:contains('Hello')")->count()
+        // );
+        // // $this->assertGreaterThan(0, $crawler->filter('h1.class')->count());
+        // $this->assertCount(1, $crawler->filter("h1"));
+        // $this->assertTrue(
+        //   $client->getResponse()->headers->contains(
+        //     'Content-Type',
+        //     'application/json'
+        //   ),
+        //   "the 'Content-Type' header is 'application/json'" //optional message shown on failuare
+        // );
+        // $this->assertContains('foo', $client->getResponse()->getContent());
+        // $link = $crawler
+        //   ->filter("a:contains('AwesomeLink')")
+        //   ->link();
+        // $crawler = $client->click($link);
+        // $this->assertStringContainsString('Remember me', $client->getResponse()->getContent());
+        //
+        // $form = $crawler->selectButton('Sign in')->form();
+        // $form['email'] = 'e@email.com';
+        // $form['password'] = '123';
+        // $crawler = $client->submit($form);
+        // $crawler = $client->followRedirect();
+        // $this->assertEquals(1, $crawler->filter('a:contains("Logout")')->count());
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+    }
+
+    public function provideUrls()
+    {
+      return [
+        ['/home'],
+        ['/login']
+      ];
     }
 }
