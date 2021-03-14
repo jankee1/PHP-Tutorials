58fce5fd85f79403aeb2f420cdab541d31243ad5
Jan <jan.blaszczyk@protonmail.com>
2021-03-14 11:27:09 +0100

    14-03-2021_practice_app

---
+++ Symfony_4_and_5-practice_app/src/Controller/FrontController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    #[Route('/', name: 'main_page')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig');
+    }
+
+    #[Route('/video-list', name: 'video_list')]
+    public function videoList(): Response
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+}
