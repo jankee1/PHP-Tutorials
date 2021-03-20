4e6008550e353b7d52958bd9f58d60cbdcceb40f
Jan <jan.blaszczyk@protonmail.com>
2021-03-20 13:26:34 +0100

    20-03-2021_adding_functionality-working_links_v4

--- Symfony_4_and_5-practice_app/src/Controller/AdminController.php
+++ Symfony_4_and_5-practice_app/src/Controller/AdminController.php
@@ -15,4 +15,34 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/my_profile.html.twig');
     }
+
+    #[Route('/categories', name: 'categories')]
+    public function categories(): Response
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    #[Route('/videos', name: 'videos')]
+    public function videos(): Response
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    #[Route('/upload_video', name: 'upload_video')]
+    public function uploadVideo(): Response
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    #[Route('/users', name: 'users')]
+    public function users(): Response
+    {
+        return $this->render('admin/users.html.twig');
+    }
+
+    #[Route('/edit_category', name: 'edit_category')]
+    public function editCategory(): Response
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
 }
