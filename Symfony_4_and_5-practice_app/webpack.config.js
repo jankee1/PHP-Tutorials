dd3dd0c859f02170d6c6b0a887919a7303191927
Jan <jan.blaszczyk@protonmail.com>
2021-03-20 13:58:04 +0100

    20-03-2021_adding_feature:webpack_installation

---
+++ Symfony_4_and_5-practice_app/webpack.config.js
@@ -0,0 +1,49 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/assets/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/assets')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    // .addEntry('app', './assets/app.js')
+    .addStyleEntry('css/dashboard', ['./assets/css/dashboard.css'])
+    .addStyleEntry('css/login', ['./assets/css/login.css'])
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    // .cleanupOutputBeforeBuild()
+    // .enableBuildNotifications()
+    // .enableSourceMaps(!Encore.isProduction())
+    // // enables hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning(Encore.isProduction())
+
+    // enables @babel/preset-env polyfills
+    // .configureBabelPresetEnv((config) => {
+    //     config.useBuiltIns = 'usage';
+    //     config.corejs = 3;
+    // })
+
+;
+
+module.exports = Encore.getWebpackConfig();
