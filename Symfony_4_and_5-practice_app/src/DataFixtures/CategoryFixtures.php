3859895f1a949eb5df3c1afb0870095165a0bc6c
Jan <jan.blaszczyk@protonmail.com>
2021-03-27 17:45:14 +0100

    27-03-2021_added_features:fixtures_v1

--- Symfony_4_and_5-practice_app/src/DataFixtures/CategoryFixtures.php
+++ Symfony_4_and_5-practice_app/src/DataFixtures/CategoryFixtures.php
@@ -11,6 +11,7 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $this->loadMainCategories($manager);
+        $this->loadSubcategories($manager, 'Electronics', 1);
     }
 
     private function loadMainCategories($manager)
@@ -24,6 +25,22 @@ class CategoryFixtures extends Fixture
         $manager->flush();
     }
 
+    private function loadSubcategories($manager, $category, $parent_id)
+    {
+      $parent = $manager->getRepository(Category::class)->find($parent_id);
+      $methodName = "get{$category}Data";
+
+      foreach($this->$methodName() as [$name]) {
+
+        $category = new Category();
+        $category->setName($name);
+        $category->setParent($parent);
+        $manager->persist($category);
+      }
+
+      $manager->flush();
+    }
+
     private function getMainCategoriesData()
     {
       return [
@@ -31,6 +48,15 @@ class CategoryFixtures extends Fixture
         ['Toys', 2],
         ['Books', 3],
         ['Movies', 4]
+      ];
+    }
+
+    private function getElectronicsData()
+    {
+      return [
+        ['Cameras', 5 ],
+        ['Computers', 6 ],
+        ['Cell Phones', 7 ]
       ];
     }
 }
