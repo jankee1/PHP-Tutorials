96ab55665ba9e5863be953182135753daefd383c
Jan <jan.blaszczyk@protonmail.com>
2021-04-11 14:51:51 +0200

    11-04-2021_added_functionalities:functional_testing_of_categories_v2

--- Symfony_4_and_5-practice_app/tests/Controllers/AdminControllerCategoriesTest.php
+++ Symfony_4_and_5-practice_app/tests/Controllers/AdminControllerCategoriesTest.php
@@ -11,11 +11,47 @@ class AdminControllerCategoriesTest extends WebTestCase
     {
       parent::setUp();
       $this->client = static::createClient();
+      $this->client->disableReboot();
+
+      $this->em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+      $this->em->beginTransaction();
+      $this->em->getConnection()->setAutoCommit(false);
     }
+
+    public function tearDown(): void
+    {
+      parent::tearDown();
+      $this->em->rollback();
+      $this->em->close();
+      $this->em = null; // avoid memory leaks
+    }
+
     public function testTextOnPage(): void
     {
         $crawler = $this->client->request('GET', '/admin/categories');
         $this->assertSame('Categories list', $crawler->filter('h2')->text());
-        $this->assertContains('Electronics', $this->client->getResponse()->getContent());
+        $this->assertStringContainsString('Electronics', $this->client->getResponse()->getContent());
+    }
+
+    public function testNumberOfItems()
+    {
+      $crawler = $this->client->request('GET', '/admin/categories');
+      $this->assertCount(21, $crawler->filter('option'));
+    }
+
+    public function testNewCategory()
+    {
+      $crawler = $this->client->request('GET', '/admin/categories');
+
+      $form = $crawler->selectButton('Add')->form([
+        'category[parent]' => 1,
+        'category[name]' => 'Other electronics'
+      ]);
+      $this->client->submit($form);
+
+      $category = $this->em->getRepository(Category::class)->findOneBy(['name' => 'Other electronics']);
+
+      $this->assertNotNull($category);
+      $this->assertSame('Other electronics', $category->getName());
     }
 }
