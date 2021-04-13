4867dddea278d9126fc49b3f29b537bc44419b0f
Jan <jan.blaszczyk@protonmail.com>
2021-04-13 18:46:00 +0200

    13-04-2021_added_functionality:sort_videos

--- Symfony_4_and_5-practice_app/src/Repository/VideoRepository.php
+++ Symfony_4_and_5-practice_app/src/Repository/VideoRepository.php
@@ -21,11 +21,13 @@ class VideoRepository extends ServiceEntityRepository
         $this->paginator = $paginator;
     }
 
-    public function findByChildIds(array $value, int $page)
+    public function findByChildIds(array $value, int $page, ?string $sort_method)
     {
+      $sort_method = $sort_method != 'rating' ? $sort_method : 'ASC';
       $dbquery = $this->createQueryBuilder('v')
         ->andWhere('v.category IN (:val)')
         ->setParameter('val', $value)
+        ->orderBy('v.title', $sort_method)
         ->getQuery();
 
       $pagination = $this->paginator->paginate($dbquery, $page, 5);
