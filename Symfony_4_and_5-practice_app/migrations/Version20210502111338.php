a0501b15023587368c713d231f9968065bc48157
Jan <jan.blaszczyk@protonmail.com>
2021-05-02 13:14:32 +0200

    02-05-2021_added_feature:fixtures_for_videos

--- Symfony_4_and_5-practice_app/migrations/Version20210502111338.php
+++ Symfony_4_and_5-practice_app/migrations/Version20210502111338.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210502104015 extends AbstractMigration
+final class Version20210502111338 extends AbstractMigration
 {
     public function getDescription() : string
     {
