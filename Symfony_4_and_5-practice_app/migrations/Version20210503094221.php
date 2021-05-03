5b150a8e8b80d2354e9262dc4efca16b0b62e14c
Jan <jan.blaszczyk@protonmail.com>
2021-05-03 13:12:16 +0200

    03-05-2021_added_functionality:display_likes_and_dislikes_v2_v3_v4

--- Symfony_4_and_5-practice_app/migrations/Version20210503094221.php
+++ Symfony_4_and_5-practice_app/migrations/Version20210503094221.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210502111338 extends AbstractMigration
+final class Version20210503094221 extends AbstractMigration
 {
     public function getDescription() : string
     {
