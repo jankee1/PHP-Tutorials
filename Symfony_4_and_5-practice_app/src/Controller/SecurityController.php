b2fad3891fab0145f4b23c1a1577747ce6ee6f20
Jan <jan.blaszczyk@protonmail.com>
2021-05-30 12:30:26 +0200

    30-05-2021_added_functionality:register_with_proper_subscription_plan_v3

--- Symfony_4_and_5-practice_app/src/Controller/SecurityController.php
+++ Symfony_4_and_5-practice_app/src/Controller/SecurityController.php
@@ -6,47 +6,84 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 use App\Entity\User;
 use App\Form\UserType;
+
+
 use App\Entity\Subscription;
 
-class SecurityController extends AbstractController {
 
-  /**
- * @Route("/register/{plan}", name="register", defaults={"plan": null})
- */
-  public function register(UserPasswordEncoderInterface $password_encoder, Request $request, SessionInterface $session, $plan)
-  {
-    if( $request->isMethod('GET')  )
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $helper)
     {
-        $session->set('planName',$plan);
-        $session->set('planPrice', Subscription::getPlanDataPriceByName($plan));
+        return $this->render('front/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
+        ]);
     }
-      $user = new User;
-      $form = $this->createForm(UserType::class, $user);
-      $form->handleRequest($request);
-      if ($form->isSubmitted() && $form->isValid())
-      {
-          $entityManager = $this->getDoctrine()->getManager();
 
-          $user->setName($request->request->get('user')['name']);
-          $user->setLastName($request->request->get('user')['last_name']);
-          $user->setEmail($request->request->get('user')['email']);
-          $password = $password_encoder->encodePassword($user, $request->request->get('user')['password']['first']);
-          $user->setPassword($password);
-          $user->setRoles(['ROLE_USER']);
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
 
-          $entityManager->persist($user);
-          $entityManager->flush();
+    /**
+     * @Route("/register/{plan}", name="register", defaults={"plan": null})
+     */
+    public function register(UserPasswordEncoderInterface $password_encoder, Request $request, SessionInterface $session, $plan)
+    {
+        if( $request->isMethod('GET')  )
+        {
+            $session->set('planName',$plan);
+            $session->set('planPrice', Subscription::getPlanDataPriceByName($plan));
+        }
 
-          $this->loginUserAutomatically($user, $password);
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
 
-          return $this->redirectToRoute('admin_main_page');
-      }
+            $user->setName($request->request->get('user')['name']);
+            $user->setLastName($request->request->get('user')['last_name']);
+            $user->setEmail($request->request->get('user')['email']);
+            $password = $password_encoder->encodePassword($user, $request->request->get('user')['password']['first']);
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+
+            $date = new \Datetime();
+            $date->modify('+1 month');
+            $subscription = new Subscription();
+            $subscription->setValidTo($date);
+            $subscription->setPlan($session->get('planName'));
+            $subscription->setFreePlanUsed(false);
+            if($plan == Subscription::getPlanDataNameByIndex(0)) // free plan
+            {
+                $subscription->setFreePlanUsed(true);
+                $subscription->setPaymentStatus('paid');
+            }
+            $user->setSubscription($subscription);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->loginUserAutomatically($user, $password);
+
+            return $this->redirectToRoute('admin_main_page');
+        }
 
         if($this->isGranted('IS_AUTHENTICATED_REMEMBERED') && $plan == Subscription::getPlanDataNameByIndex(0)) // free plan
         {
@@ -58,36 +95,20 @@ class SecurityController extends AbstractController {
         {
             return $this->redirectToRoute('payment');
         }
-        
-      return $this->render('front/register.html.twig',['form'=>$form->createView()]);
-  }
 
-  /**
-   * @Route("/login", name="login")
-   */
-  public function login(AuthenticationUtils $helper)
-  {
-      return $this->render('front/login.html.twig', [
-          'error' => $helper->getLastAuthenticationError()
-          ]);
-  }
+        return $this->render('front/register.html.twig',['form'=>$form->createView()]);
+    }
 
-  /**
-   * @Route("/logout", name="logout")
-   */
-  public function logout() : void
-  {
-      throw new \Exception('This should never be reached!');
-  }
-  private function loginUserAutomatically($user, $password)
-  {
-      $token = new UsernamePasswordToken(
-          $user,
-          $password,
-          'main', // security.yaml
-          $user->getRoles()
-      );
-      $this->get('security.token_storage')->setToken($token);
-      $this->get('session')->set('_security_main',serialize($token));
-  }
+
+    private function loginUserAutomatically($user, $password)
+    {
+        $token = new UsernamePasswordToken(
+            $user,
+            $password,
+            'main', // security.yaml
+            $user->getRoles()
+        );
+        $this->get('security.token_storage')->setToken($token);
+        $this->get('session')->set('_security_main',serialize($token));
+    }
 }
