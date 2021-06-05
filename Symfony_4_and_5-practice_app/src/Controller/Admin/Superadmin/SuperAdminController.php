1e769ebb20cd5bc6e036fd72c853c510ecd66690
Jan <jan.blaszczyk@protonmail.com>
2021-06-05 19:34:03 +0200

    05-06-2021_added_functionality:set_duration_for_uploaded_video

--- Symfony_4_and_5-practice_app/src/Controller/Admin/Superadmin/SuperAdminController.php
+++ Symfony_4_and_5-practice_app/src/Controller/Admin/Superadmin/SuperAdminController.php
@@ -142,4 +142,90 @@ class SuperAdminController extends AbstractController
          }
          return $this->render('admin/upload_video_vimeo.html.twig');
      }
+
+     /**
+      * @Route("/upload-video-by-vimeo", name="upload_video_by_vimeo")
+     */
+     public function uploadVideoByVimeo(Request $request)
+     {
+         $vimeo_id = preg_replace('/^\/.+\//','',$request->get('video_uri'));
+         if($request->get('videoName') && $vimeo_id)
+         {
+             $em = $this->getDoctrine()->getManager();
+             $video = new Video();
+             $video->setTitle($request->get('videoName'));
+             $video->setPath(Video::VimeoPath.$vimeo_id);
+
+             $em->persist($video);
+             $em->flush();
+
+             return $this->redirectToRoute('videos');
+         }
+         return $this->render('admin/upload_video_vimeo.html.twig');
+     }
+
+
+     /**
+      * @Route("/set-video-duration/{video}/{vimeo_id}", name="set_video_duration", requirements={"vimeo_id"=".+"})
+     */
+     public function setVideoDuration(Video $video, $vimeo_id)
+     {
+         if( !is_numeric($vimeo_id) )
+         {
+             // you can handle here setting duration for locally stored files
+             // ....
+             return $this->redirectToRoute('videos');
+         }
+
+         $user_vimeo_token = $this->getUser()->getVimeoApiKey();
+         $curl = curl_init();
+
+         curl_setopt_array($curl, array(
+             CURLOPT_URL => "https://api.vimeo.com/videos/{$vimeo_id}",
+             CURLOPT_RETURNTRANSFER => true,
+             CURLOPT_ENCODING => "",
+             CURLOPT_MAXREDIRS => 10,
+             CURLOPT_TIMEOUT => 30,
+             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+             CURLOPT_CUSTOMREQUEST => "GET",
+             CURLOPT_HTTPHEADER => array(
+                 "Accept: application/vnd.vimeo.*+json;version=3.4",
+                 "Authorization: Bearer $user_vimeo_token",
+                 "Cache-Control: no-cache",
+                 "Content-Type: application/x-www-form-urlencoded"
+             ),
+         ));
+
+         $response = curl_exec($curl);
+         $err = curl_error($curl);
+
+         curl_close($curl);
+
+         if ($err)
+         {
+             throw new ServiceUnavailableHttpException('Error. Try again later. Message: '.$err);
+         }
+         else
+         {
+             $duration =  json_decode($response, true)['duration'] / 60;
+
+             if($duration)
+             {
+                 $video->setDuration($duration);
+                 $em = $this->getDoctrine()->getManager();
+                 $em->persist($video);
+                 $em->flush();
+             }
+             else
+             {
+                 $this->addFlash(
+                     'danger',
+                     'We were not able to update duration. Check the video.'
+                 );
+             }
+
+             return $this->redirectToRoute('videos');
+         }
+
+     }
 }
