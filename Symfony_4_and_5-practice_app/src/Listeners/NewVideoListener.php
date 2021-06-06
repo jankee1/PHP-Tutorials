5063d668f17dd268acd2cd99266041579bfacf0f
Jan <jan.blaszczyk@protonmail.com>
2021-06-06 11:13:52 +0200

    05-06-2021_added_functionality:Send_email_on_the_event_of_adding_a_new_video_upd

--- Symfony_4_and_5-practice_app/src/Listeners/NewVideoListener.php
+++ Symfony_4_and_5-practice_app/src/Listeners/NewVideoListener.php
@@ -8,26 +8,46 @@ use App\Entity\User;
 
 class NewVideoListener
 {
+    // public function __construct(\Twig_Environment $templating, \Swift_Mailer $mailer)
+    // {
+    //   $this->templating = $templating;
+    //   $this->mailer = $mailer;
+    // }
 
     public function postPersist(LifecycleEventArgs $args)
     {
-        $entity = $args->getObject();
-
-        // only act on some "Product" entity
-        if (!$entity instanceof Video) {
-            return;
-        }
-
-
-        $entityManager = $args->getObjectManager();
-        // ... do something with the Product
-
-        $users = $entityManager->getRepository(User::class)->findAll();
-
-       foreach($users as $user)
-        {
-            exit( $user->getName().' '.$entity->getTitle());
-        }
+       //
+       //  $entity = $args->getObject();
+       //
+       //  // only act on some "Product" entity
+       //  if (!$entity instanceof Video) {
+       //      return;
+       //  }
+       //
+       //
+       //  $entityManager = $args->getObjectManager();
+       //  // ... do something with the Product
+       //
+       //  $users = $entityManager->getRepository(User::class)->findAll();
+       //
+       // foreach($users as $user)
+       //  {
+       //      $message = (new \Swift_Message('Hello Email'))
+       //          ->setFrom('send@example.com')
+       //          ->setTo($user->getEmail())
+       //          ->setBody(
+       //              $this->templating->render(
+       //                  'emails/new_video.html.twig',
+       //                  [
+       //                      'name' => $user->getName(),
+       //                      'video' => $entity
+       //                  ]
+       //              ),
+       //              'text/html'
+       //          );
+       //
+       //      $this->mailer->send($message);
+       //  }
 
     }
 }
